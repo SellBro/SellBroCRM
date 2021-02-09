2b97e78d4116012e7c143348bce3d4b7fcfc6560
youngrake <youngrake@protonmail.com>
2021-02-09 16:31:47 +0200

    appWidget layout

--- MainWidget.cpp
+++ MainWidget.cpp
@@ -1,11 +1,14 @@
 #include "MainWidget.h"
+#include <QTableWidget>
 
 MainWidget::MainWidget(QStackedWidget *parent)
 	: QStackedWidget(parent)
 {
 	ui.setupUi(this);
 
-	 // Qt::WindowFlags flags = this->windowFlags(); this->setWindowFlags(flags | Qt::FramelessWindowHint); // remove border
+
+
+	// Qt::WindowFlags flags = this->windowFlags(); this->setWindowFlags(flags | Qt::FramelessWindowHint); // remove border
 }
 
 MainWidget::~MainWidget()
--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 #include <QtWidgets/QApplication>
 
 
-
 int main(int argc, char *argv[])
 {
-
     QApplication a(argc, argv);
 
     MainWidget mainWidget;
+   
 
     mainWidget.show();
     return a.exec();
